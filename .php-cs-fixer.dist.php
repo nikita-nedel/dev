61d604baea3773776e750e9148e2f377f2632a7c
Nikita <nedelka999@gmail.com>
2025-06-30 03:02:04 +0300

    PHPCsFixed added

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,14 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude(['var', 'tests', 'vendor'])
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+        '@PSR12' => true,
+    ])
+    ->setFinder($finder)
+;
